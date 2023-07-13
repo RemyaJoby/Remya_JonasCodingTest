3bc8882f7019f9330832a2d7f19cc15b073658f7
RemyaJoby <remyajames@gmail.com>
2023-07-13 07:02:19 -0600

    Create employee controller  Get methods

---
+++ BusinessLayer/Services/EmployeeService.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer.Model.Interfaces;
+using System.Collections.Generic;
+using AutoMapper;
+using BusinessLayer.Model.Models;
+using DataAccessLayer.Model.Interfaces;
+using DataAccessLayer.Model.Models;
+using System.Globalization;
+
+namespace BusinessLayer.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IMapper _mapper;
+
+        public EmployeeService(IEmployeeRepository employeeRepository, IMapper mapper)
+        {
+            _employeeRepository = employeeRepository;
+            _mapper = mapper;
+        }
+        public IEnumerable<EmployeeInfo> GetAllEmployees()
+        {
+            var res = _employeeRepository.GetAll();
+            return _mapper.Map<IEnumerable<EmployeeInfo>>(res);
+        }
+
+        public EmployeeInfo GetEmployeeByCode(string employeeCode)
+        {
+            var result = _employeeRepository.GetByCode(employeeCode);
+            return _mapper.Map<EmployeeInfo>(result);
+        }
+    }
+}
---
+++ WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using AutoMapper;
+using BusinessLayer.Model.Interfaces;
+using BusinessLayer.Model.Models;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        private readonly IEmployeeService _employeeService;
+        private readonly IMapper _mapper;
+
+        public EmployeeController(IEmployeeService employeeService, IMapper mapper)
+        {
+            _employeeService = employeeService;
+            _mapper = mapper;
+        }
+        // GET api/<controller>
+        public IEnumerable<EmployeeDto> GetAll()
+        {
+            var items = _employeeService.GetAllEmployees();
+            return _mapper.Map<IEnumerable<EmployeeDto>>(items);
+        }
+
+        // GET api/<controller>/5
+        public EmployeeDto Get(string employeeCode)
+        {
+            var item = _employeeService.GetEmployeeByCode(employeeCode);
+            return _mapper.Map<EmployeeDto>(item);
+        }
+    }
+}

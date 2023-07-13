e71178e43cf1f1031bc6927cd8bc0bf0c0e89205
RemyaJoby <remyajames@gmail.com>
2023-07-13 07:23:35 -0600

    Logging functionality

--- WebApi/Controllers/CompanyController.cs
+++ WebApi/Controllers/CompanyController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.UI.WebControls;
 using AutoMapper;
 using BusinessLayer.Model.Interfaces;
 using BusinessLayer.Model.Models;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -13,37 +16,89 @@ namespace WebApi.Controllers
     {
         private readonly ICompanyService _companyService;
         private readonly IMapper _mapper;
+        private readonly ILogger _logger;
 
-        public CompanyController(ICompanyService companyService, IMapper mapper)
+        public CompanyController(ICompanyService companyService, IMapper mapper,ILogger _log )
         {
             _companyService = companyService;
             _mapper = mapper;
+            _logger = _log;
         }
         // GET api/<controller>
         public async Task<IEnumerable<CompanyDto>> GetAll()
         {
-            var items = await _companyService.GetAllCompanies();
-            return _mapper.Map<IEnumerable<CompanyDto>>(items);
+            try
+            {
+                var items = await _companyService.GetAllCompanies();
+                return _mapper.Map<IEnumerable<CompanyDto>>(items);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return null;
+            }
+           
         }
 
         // GET api/<controller>/5
         public async Task<CompanyDto> Get(string companyCode)
         {
-            var item = await _companyService.GetCompanyByCode(companyCode);
-            return _mapper.Map<CompanyDto>(item);
+            try
+            {
+                var item = await _companyService.GetCompanyByCode(companyCode);
+                return _mapper.Map<CompanyDto>(item);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return null;
+            }
+            
         }
 
         public async Task<bool> Post([FromBody] string value)
         {
+            try
+            {
+                return await _companyService.SaveCompany(_mapper.Map<CompanyInfo>(value));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return false;
+            }
             // create
-            return await _companyService.SaveCompany(_mapper.Map<CompanyInfo>(value));
+           
         }
 
         // PUT api/<controller>/5
         public async Task<bool> Put([FromBody] CompanyDto value)
         {
             //Save is handling both insert and update.
-            return await _companyService.SaveCompany(_mapper.Map<CompanyInfo>(value));
+            try
+            {
+                return await _companyService.SaveCompany(_mapper.Map<CompanyInfo>(value));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return false;
+            }
+          
+        }
+
+        public async Task<bool> Delete(string companyCode)
+        {
+            try
+            {
+                return await _companyService.DeleteCompany(companyCode);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return false;
+            }
+            
         }
 
     }
--- WebApi/Controllers/EmployeeController.cs
+++ WebApi/Controllers/EmployeeController.cs
@@ -4,6 +4,7 @@ using System.Web.Http;
 using AutoMapper;
 using BusinessLayer.Model.Interfaces;
 using BusinessLayer.Model.Models;
+using Microsoft.Extensions.Logging;
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -12,24 +13,44 @@ namespace WebApi.Controllers
     {
         private readonly IEmployeeService _employeeService;
         private readonly IMapper _mapper;
+        private readonly ILogger _logger;
 
-        public EmployeeController(IEmployeeService employeeService, IMapper mapper)
+        public EmployeeController(IEmployeeService employeeService, IMapper mapper, ILogger logger)
         {
             _employeeService = employeeService;
             _mapper = mapper;
+            _logger = logger;
         }
         // GET api/<controller>
         public IEnumerable<EmployeeDto> GetAll()
         {
-            var items = _employeeService.GetAllEmployees();
-            return _mapper.Map<IEnumerable<EmployeeDto>>(items);
+            try
+            {
+                var items = _employeeService.GetAllEmployees();
+                return _mapper.Map<IEnumerable<EmployeeDto>>(items);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return null;
+            }
+
         }
 
         // GET api/<controller>/5
         public EmployeeDto Get(string employeeCode)
         {
-            var item = _employeeService.GetEmployeeByCode(employeeCode);
-            return _mapper.Map<EmployeeDto>(item);
+            try
+            {
+                var item = _employeeService.GetEmployeeByCode(employeeCode);
+                return _mapper.Map<EmployeeDto>(item);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return null;
+            }
+            
         }
     }
 }
